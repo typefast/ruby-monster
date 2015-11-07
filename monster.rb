774eb195c95ccff5e46b29049fe38c71a495a4b8
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-07 12:52:15 +0000

    Add roar method

--- monster.rb
+++ monster.rb
@@ -1,15 +1,28 @@
 class Monster
-  attr_reader :name
+  attr_reader :name, :actions
   
   def initialize(name)
     @name = name
+    @actions = {
+      roar: 0,
+      scares: 0
+    }
   end  
   
   def say(&block)
     print "#{name} says..."
     yield
   end
+  
+  def roar(&block)
+    actions[:roar] += 1
+    print "#{name} shouts! "
+    yield
+  end
 end
 
 monster = Monster.new("Arnold")
 monster.say { puts "Welcome rargh" }
+monster.roar do 
+  puts "Rarghhhhhh"
+end
